e92556cbc12cf57a0a8daead167a5207e3ad5815
andres briñez <brinezlopez08@gmail.com>
2023-06-21 14:39:15 -0500

    Código del paso 3

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 
+import React, { useState, useEffect } from "react";
+import { Header } from "./components/Header";
+import Draggable from 'react-draggable';
+
 function App() {
   return (
     <div className="App"></div>);
